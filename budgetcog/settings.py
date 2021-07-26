0f961084d33ee030656c90352e08bb4726f74b75
Sam Thompson <samthompson292@gmail.com>
2021-07-26 17:44:26 +0100

    added db

--- budgetcog/settings.py
+++ budgetcog/settings.py
@@ -60,12 +60,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'budgetcog.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'budgetcog',
+            'USER': 'samthompson',
+            'PASSWORD': '%mh9]#HDM&qp}2bq',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
